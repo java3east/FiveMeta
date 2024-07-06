0a58511e8e4cd0e9f6f1ee73fce21b94a17dad93
java3east <beastofjava7@gmail.com>
2024-07-06 18:22:54 +0200

    06.07.2024

--- import/meta_qbcore.lua
+++ import/meta_qbcore.lua
@@ -42,8 +42,25 @@ function SV_QBCore.Functions.GetPlayer(serverId) end
 ---@param cb fun(source: player, cb: fun(...), ...: any) the callback function
 function SV_QBCore.Functions.CreateCallback(name, cb) end
 
+---Notifies the player with a message.
+---@param player player the server id of the player
+---@param message string the message to notify the player with
+---@param type string the type of the notification
+function SV_QBCore.Functions.Notify(player, message, type) end
+
 ---@class SV_QBCore.Player
 local SV_Player = {}
+
+---@class SV_QBCore.Player.Functions
+SV_Player.Functions = {}
+
+---Removes the given amount of money from the given account of the player.
+---@param account string the name of the account
+---@param amount integer the amount of money to remove
+---@param reason string? the reason for removing the money
+---@return boolean success (I guess. There is no information given, except that it dose return a boolean. This documentation is BAD!)
+function SV_Player.Functions.RemoveMoney(account, amount, reason) end
+
 
 ---@class SV_QBCore.Player.PlayerData
 SV_Player.PlayerData = {}
@@ -57,6 +74,27 @@ SV_Player.PlayerData.job = {}
 ---@type string the name of the job
 SV_Player.PlayerData.job.name = ""
 
+---@class SV_QBCore.Player.PlayerData.accounts
+SV_Player.PlayerData.accounts = {}
+
+---@type integer the amount of money in the bank account
+SV_Player.PlayerData.accounts.bank = 0
+
+---@type integer the amount of money in the cash account
+SV_Player.PlayerData.accounts.cash = 0
+
+---@type integer the amount of money in the crypto account
+SV_Player.PlayerData.accounts.crypto = 0
+
 
 ---@class CL_QBCore
 CL_QBCore = {}
+
+---@class CL_QBCore.Functions
+CL_QBCore.Functions = {}
+
+---Triggers a server callback with the given name and arguments.
+---@param name string the name of the callback
+---@param cb fun(...: any) the callback function
+---@vararg any the arguments for the callback
+function CL_QBCore.Functions.TriggerCallback(name, cb, ...) end
